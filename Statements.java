8381c3a77ff29c1a2272d0d55d9ecdee7512e1e5
ezhilmathiyalagan <ezhilmathiyalagan@gmail.com>
2018-05-29 12:34:52 +0530

    if Statements

---
+++ Statements.java
@@ -0,0 +1,12 @@
+public class Statements
+{
+public static void main(String[] args)
+{
+int count1=10;
+int count2=20;
+if(count1<count2)
+System.out.println("yes");
+else
+System.out.println("no");
+}
+}
